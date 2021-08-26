950b928b5d5ccf4f9ea18557f4e4b48f775c574e
Jordan Reger <jordanreger@gmail.com>
2021-08-26 12:56:32 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,9 +14,6 @@ class App extends LitElement {
         <latt-route path="/test">
           test (hello from /asdf!)
         </latt-route>
-        <latt-route path="/pee">
-          <latt-redirect to="/test" />
-        </latt-route>
         <latt-catch to="/" />
       </latt-router>
     `;
